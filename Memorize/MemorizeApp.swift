ee3ba816f0ba43098a0f199433e24b4b53ef6b74
Patrick Frei <p_frei@outlook.com>
2021-11-13 04:45:48 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Patrick Frei on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
