1181c2e3e32a7c6589c920d51fbbf89d6760bd45
Patrick Frei <p_frei@outlook.com>
2021-11-13 04:52:37 +0100

    small change in text display

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("pie, world!")
             .padding()
+            .background().colorInvert()
     }
 }
 
